5572efb1fad54a346dc82a18a8f240ecd464df9e
yyamada12 <12yacropolisy@gmail.com>
2019-05-18 17:14:52 +0900

    [add] result view controller

---
+++ NameScoreApp/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  NameScoreApp
+//
+//  Created by Yoshinobu Yamada on 2019/05/18.
+//  Copyright © 2019 Yoshinobu Yamada. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
